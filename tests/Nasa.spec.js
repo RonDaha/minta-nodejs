46dc102b1cd375066bc62be8046467c48684c28d
Ron Dahan <rond@dpogroup.com>
2021-08-31 19:34:03 +0200

    resolve test issue

---
+++ tests/Nasa.spec.js
@@ -0,0 +1,11 @@
+/*  */
+
+import { Nasa } from '../src/domains/Nasa/Nasa'
+
+/* Testing the Nasa API is up and response the same data structure */
+test('Nasa API is up', () => {
+    console.log(Nasa)
+    // const meteor = new Meteor()
+    // meteor.init()
+    // expect(sum(1, 2)).toBe(3)
+})
