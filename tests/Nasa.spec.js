b0a4199cdaaf2b648a891f0c100682d41cca2c93
Ron Dahan <rond@dpogroup.com>
2021-08-31 21:26:59 +0200

    tests v2

--- tests/Nasa.spec.js
+++ tests/Nasa.spec.js
@@ -2,30 +2,30 @@ import { Nasa } from '../src/domains/Nasa/Nasa'
 import { Meteor } from '../src/domains/Nasa/Meteor'
 import { Weather } from '../src/domains/Nasa/Weather'
 
-
-/* Testing the Nasa API is up and response the same data structure */
+/* Testing the Nasa API is up and response the expected data structure */
 describe('Testing the Nasa Domain', () => {
-
 
     test('Nasa API is up and the data manipulation is working as expected', async () => {
 
-        /* Make sure the app initialize successfully */
+        /* Make sure the app initialize successfully (the Nasa API is up) */
         const nasa = new Nasa()
         await nasa.init()
 
         const meteor = new Meteor()
-        const weather = new Weather()
 
         /* Test Filter #1 */
         const dataFilteredByYear = meteor.getMeteorData({ year: 1994 })
+        expect(Array.isArray(dataFilteredByYear)).toBe(true)
         expect(dataFilteredByYear.length).toEqual(6)
 
         /* Test Filter #2 */
         const dataFilteredByYearAndMass = meteor.getMeteorData({ year: 1994, mass: 1000 })
+        expect(Array.isArray(dataFilteredByYearAndMass)).toBe(true)
         expect(dataFilteredByYearAndMass.length).toEqual(5)
 
-        /* Test without filters and the 'HWS' is added */
+        /* Test without filters and that the 'HWS' property has been added */
         const dataWithWeather = await nasa.getMeteorDataWithWeather({})
+        expect(Array.isArray(dataWithWeather)).toBe(true)
         expect(dataWithWeather[0]).toHaveProperty('HWS')
 
     })
